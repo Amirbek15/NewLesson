78bec2de307d86b8768925e7a2a811b1e9f286fa
Amirbek Shipulin <amirbek.shipulin@mail.ru>
2023-02-10 14:14:55 +0400

    Exmaple005 new

---
+++ Example005new/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя:");
+string username = Console.ReadLine();
+if(username.ToLower() == "John")
+{
+    Console.WriteLine("Ypa,это же JOHN");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
